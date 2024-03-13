de2dd4c337f5bb9ddd370b57ac64d4019826279e
Ryan <rhuynhr@gmail.com>
2024-03-12 21:01:02 -0700

    Added API into script.js and added some lines into index.html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+const settings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ba151ed912msh57f6efac4ca623cp1768e5jsn4be1685ac01d',
+		'X-RapidAPI-Host': 'trivia-by-api-ninjas.p.rapidapi.com'
+	}
+};
 
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
