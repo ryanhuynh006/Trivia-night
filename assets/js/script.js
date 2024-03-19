ba0b0cc860b94ba040e455fdcb8af92701f30807
Jace Wilson <157767563+jacewilson04@users.noreply.github.com>
2024-03-19 00:08:29 -0400

    Made play button work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-const CATEGORY = 9
-const AMOUNT = 10
-const DIFFICULTY = "medium"
+const AMOUNT_OF_QUESTIONS = 10
 
+//Using the google api we can define the scope of our search
+//Perhaps in the future install gapi and use it to find a random scope within our data so we are not retrieving such a large amount
 const SPREAD_ID = '1axUz3XAKfIoKwvIzTotC6ix2v5PZ_ucwpv2naZeSZsg'
 const SHEET_NUMBER = '1107320111'
 const SHEET_URL = `https://docs.google.com/spreadsheets/d/${SPREAD_ID}/gviz/tq?tqx=out:json&headers=1&gid=${SHEET_NUMBER}`
@@ -11,6 +11,9 @@ const SHEET_OPTIONS = {
         'Access-Control-Allow-Origin': '*'
     },
 }
+
+const playButton = document.querySelector("#play")
+const difficultySelect = document.querySelector("#Difficulty")
 
 async function getSheetQuestions() {
     const responce = await fetch(SHEET_URL, SHEET_OPTIONS)
@@ -27,7 +30,7 @@ async function getSheetQuestions() {
 getSheetQuestions()
 
 async function getTriviaQuestions(amount, category, difficulty) {
-    const triviaUrl = 'https://opentdb.com/api.php?amount='+amount+'&category='+category+'&difficulty='+difficulty;
+    const triviaUrl = 'https://opentdb.com/api.php?type=multiple&amount='+amount+'&category='+category+'&difficulty='+difficulty;
 
     const responce = await fetch(triviaUrl)
     const result = await responce.json()
@@ -38,4 +41,11 @@ async function getTriviaQuestions(amount, category, difficulty) {
     return triviaArray
 }
 
-getTriviaQuestions(AMOUNT, CATEGORY, DIFFICULTY)
+playButton.addEventListener("click", function () {
+    const categoryOption = document.querySelector("#Categories option:checked")
+    const category = categoryOption.dataset.number
+
+    const difficulty = difficultySelect.value.toLowerCase()
+
+    getTriviaQuestions(AMOUNT_OF_QUESTIONS, category, difficulty)
+})
